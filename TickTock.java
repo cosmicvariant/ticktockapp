e6d9765a47476eb04ca25b2d4643d877fce6f4b4
cosmicvariant <85935686+cosmicvariant@users.noreply.github.com>
2023-03-24 17:59:01 +0200

    Add files via upload

---
+++ TickTock.java
@@ -0,0 +1,34 @@
+import java.awt.event.*;	
+import javax.swing.*;	
+
+public class TickTock
+{
+    public static void main(String[] args)
+    {
+        
+        Timer t = new Timer(1000, new Ticker());	
+        t.start();	
+
+        
+        JOptionPane.showMessageDialog(null, 	
+            "Click OK if you wish to exit program");
+    }
+}
+
+class Ticker implements ActionListener	
+{
+    private boolean tick = true;	
+
+    public void actionPerformed(ActionEvent event)
+    {
+        if (tick)
+        {
+            System.out.println("Tick...");	
+        }
+        else
+        {
+            System.out.println("Tock...");	
+        }
+        tick = !tick;	
+    }
+}
